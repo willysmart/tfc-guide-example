3562ac1b23671bd9745847d313f1bf0927a044c5
willysmart <willysmart@users.noreply.github.com>
2021-09-26 23:34:14 -0400

    init

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-northeast-2"
+}
+
+variable "db_table_name" {
+  type    = string
+  default = "terraform-learn"
+}
+
+variable "db_read_capacity" {
+  type    = number
+  default = 1
+}
+
+variable "db_write_capacity" {
+  type    = number
+  default = 2
+}
+
+variable "tag_user_name" {
+  type = string
+}
